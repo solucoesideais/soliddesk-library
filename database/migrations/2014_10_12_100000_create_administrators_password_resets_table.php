fcf9f54a71617b845d37c448cb8843dc1e70f687
Deleu <deleugyn@gmail.com>
2018-03-30 16:42:50 -0400

    Fix class name

--- database/migrations/2014_10_12_100000_create_administrators_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_administrators_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateAdministratorsPasswordResetsTable extends Migration
 {
     const TABLE = 'administrators_password_resets';
 
