0884545e4afaace4ead193911af6598e533bd84f
Deleu <deleugyn@gmail.com>
2018-04-05 15:38:10 -0400

    Add company id to useres table

--- database/migrations/2018_04_02_203710_create_users_table.php
+++ database/migrations/2018_04_02_203710_create_users_table.php
@@ -18,9 +18,12 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->unsignedInteger('company_id');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('company_id')->references('id')->on('companies');
         });
     }
 
