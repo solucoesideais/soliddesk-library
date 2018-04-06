3399696499fc10137326197646ab9af81ee02977
Deleu <deleugyn@gmail.com>
2018-04-06 16:22:52 -0400

    Add company id to users

--- database/factories/Auth/UserFactory.php
+++ database/factories/Auth/UserFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator as Faker;
 use Library\Eloquent\Auth\User;
+use Library\Eloquent\Company;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,6 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'company_id' => factory(Company::class)
     ];
 });
