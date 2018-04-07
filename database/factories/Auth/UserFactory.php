d4385704cf01676a7217bf2e8eb24e7e9b78bc7a
Deleu <deleugyn@gmail.com>
2018-04-07 12:25:36 -0400

    fix factory

--- database/factories/Auth/UserFactory.php
+++ database/factories/Auth/UserFactory.php
@@ -3,6 +3,7 @@
 use Faker\Generator as Faker;
 use Library\Eloquent\Auth\User;
 use Library\Eloquent\Company;
+use Library\Auth\UserType;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
-        'company_id' => factory(Company::class)
+        'company_id' => factory(Company::class),
+        'type' => $faker->randomElement(UserType::options()),
     ];
 });
