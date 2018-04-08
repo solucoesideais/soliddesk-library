65ba546e400485b9b1f42b681e6384a7ce7615e7
Deleu <deleugyn@gmail.com>
2018-04-08 06:31:01 -0400

    Improve tiemzone settings

--- src/Providers/TimezoneServiceProvider.php
+++ src/Providers/TimezoneServiceProvider.php
@@ -9,10 +9,18 @@ class TimezoneServiceProvider extends ServiceProvider
 {
     const BRAZIL_DATE_TIME_FORMAT = 'd/m/Y H:i:s';
 
+    const BRAZIL_TIMEZONE = 'America/Sao_Paulo';
+
+    const BRAZIL_LOCALE = 'pt_BR';
+
     public function boot()
     {
         Carbon::setToStringFormat(self::BRAZIL_DATE_TIME_FORMAT);
 
-        $this->app->config->set('app.timezone', 'America/Sao_Paulo');
+        $this->app->config->set('app.timezone', self::BRAZIL_TIMEZONE);
+
+        Carbon::setLocale(self::BRAZIL_LOCALE);
+
+        date_default_timezone_set(self::BRAZIL_TIMEZONE);
     }
 }
