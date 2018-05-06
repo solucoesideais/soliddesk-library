15ac5cda93b9cbcf35af7895a31b647555447757
Deleu <deleugyn@gmail.com>
2018-05-06 12:06:36 -0400

    Provide attribute for logoUrl

--- src/Eloquent/Company.php
+++ src/Eloquent/Company.php
@@ -3,7 +3,12 @@
 namespace Library\Eloquent;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+use Library\Storage\Disk;
 
+/**
+ * @property string logoUrl
+ */
 class Company extends BaseModel
 {
     use SoftDeletes;
@@ -11,5 +16,10 @@ class Company extends BaseModel
     public function departments()
     {
         return $this->belongsToMany(Department::class);
+    }
+
+    public function getLogoUrlAttribute()
+    {
+        return Storage::disk(Disk::COMPANIES)->url($this->logo);
     }
 }
